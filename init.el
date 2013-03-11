b9808ffa58091bf9e56adb782559bd4c6c5b4bf4
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2013-03-11 13:50:13 +0200

    Added virtualenv and multiplecursors

--- init.el
+++ init.el
@@ -10,12 +10,32 @@
 (add-to-list 'load-path "~/.emacs.d/plugins/yasnippet")
 (add-to-list 'load-path "~/.emacs.d/plugins/js2-mode")
 (add-to-list 'load-path "~/.emacs.d/plugins/paredit")
+(add-to-list 'load-path "~/.emacs.d/plugins/virtualenv")
 (add-to-list 'load-path "~/.emacs.d/plugins/distel/elisp")
+(add-to-list 'load-path "~/.emacs.d/plugins/multiplecursors")
 ;;;;;;;;;;;;;;;;;;;;;
 ;; disable toolbar ;;
 ;;;;;;;;;;;;;;;;;;;;;
 (tool-bar-mode -1)
 
+;;;;;;;;;;;;;;;;;;;;;;;
+;; Enable virtualenv ;;
+;;;;;;;;;;;;;;;;;;;;;;;
+(require 'virtualenv)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Enable multiple cursors ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'multiple-cursors)
+;; When you have an active region that spans multiple lines,
+;; the following will add a cursor to each line:
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+;; When you want to add multiple cursors not based on continuous lines,
+;; but based on keywords in the buffer, use:
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 ;;;;;;;;;;;;;;;;;;;;;
 ;; enable ido mode ;;
 ;;;;;;;;;;;;;;;;;;;;;
@@ -182,6 +202,11 @@
 ;;;;;;;;;;;;;;;;
 (require 'distel)
 (distel-setup)
+
+
+;;;;;;;;;;;;;;;;;;;;;;
+;; multiple cursors ;;
+;;;;;;;;;;;;;;;;;;;;;;
 
 ;; http://bc.tech.coop/blog/070528.html
 ;; Some Erlang customizations
