7fd4da8733551da98e039085c2849bc5f41451c3
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2013-03-12 17:11:29 +0200

    Removed nxhtml mode and added webmode

--- init.el
+++ init.el
@@ -13,6 +13,8 @@
 (add-to-list 'load-path "~/.emacs.d/plugins/virtualenv")
 (add-to-list 'load-path "~/.emacs.d/plugins/distel/elisp")
 (add-to-list 'load-path "~/.emacs.d/plugins/multiplecursors")
+(add-to-list 'load-path "~/.emacs.d/plugins/webmode")
+
 ;;;;;;;;;;;;;;;;;;;;;
 ;; disable toolbar ;;
 ;;;;;;;;;;;;;;;;;;;;;
@@ -23,6 +25,27 @@
 ;;;;;;;;;;;;;;;;;;;;;;;
 (require 'virtualenv)
 
+;;;;;;;;;;;;;;;;;;;;;
+;; Enable web-mode ;;
+;;;;;;;;;;;;;;;;;;;;;
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+(defun web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+)
+(add-hook 'web-mode-hook  'web-mode-hook)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Enable multiple cursors ;;
@@ -108,7 +131,7 @@
 ;;;;;;;;;;;;;;;;
 ;; add nxhtml ;;
 ;;;;;;;;;;;;;;;;
-(load "~/.emacs.d/plugins/nxhtml/autostart.el")
+;; (load "~/.emacs.d/plugins/nxhtml/autostart.el")
 
 ;;;;;;;;;;;;;;;;;;;;;;
 ;; enable yasnippet ;;
